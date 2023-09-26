e115003f8a14dfeb5a3842356a833dc133e5bac2
gtcelvam <selvasizzler@gmail.com>
2023-09-26 20:22:44 +0530

    Cors Error Fixed : changes base url from http to https.

--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -4,7 +4,7 @@ import { TwelveHourWeatherType } from "../types/forcast";
 export const USER_URL = "https://jsonplaceholder.typicode.com/users";
 
 export const ACCUWEATHER_KEY = process.env.NEXT_PUBLIC_ACCUWEATHER_API_KEY;
-export const ACCUWEATHER_URL = "http://dataservice.accuweather.com";
+export const ACCUWEATHER_URL = "https://dataservice.accuweather.com";
 export const ACCUWEATHER_ICON_URL = (key: number) =>
   `https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/${
     key < 10 ? "0" + key : key
